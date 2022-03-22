62262408b8a281b9b08839b39cdc227b4ec9f4c2
Vlad Nechyporenko <vlladdd28@gmail.com>
2022-03-22 19:41:31 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Vlad Nechyporenko on 22.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
